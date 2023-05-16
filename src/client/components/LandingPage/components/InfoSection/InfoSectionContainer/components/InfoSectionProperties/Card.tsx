ed0d4c1d0324e8d3277306d1bace6784e041f5b7
Ushie01 <abugodwinaj@gmail.com>
2023-05-17 00:46:19 +0100

    feat: Info Section Properties[PL-14]

--- src/client/components/LandingPage/components/InfoSection/InfoSectionContainer/components/InfoSectionProperties/Card.tsx
+++ src/client/components/LandingPage/components/InfoSection/InfoSectionContainer/components/InfoSectionProperties/Card.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 const Card = () => {
 	return (
-		<div className='flex flex-row items-center justify-between'>
+		<div className='flex flex-col justify-center lg:flex-row items-center lg:justify-between'>
 			{CLIENT_PROPERTY.map((value, index) => (
 				<div
 					key={index}
-					className='flex flex-col items-center justify-center space-y-10 rounded-lg text-center'
+					className='flex flex-col items-center justify-center 2sm:mt-16 lg:mt-0 space-y-10 rounded-lg text-center'
 				>
 					<Image
 						src={value.image}
@@ -16,10 +16,10 @@ const Card = () => {
 						alt='svg'
 						className='rounded-md duration-150 hover:scale-105'
 					/>
-					<p className='text-wrap m-auto mt-2 text-xl font-bold'>
+					<p className='text-wrap m-auto mt-2 text-2xl font-bold'>
 						{value.text}
 					</p>
-					<button className='rounded-full border-2 p-5 text-gray-500 duration-150 hover:scale-105'>
+					<button className='rounded-full border-2 w-52 p-5 text-gray-500 duration-150 hover:scale-105'>
 						263 Properties
 					</button>
 				</div>
--- src/client/components/LandingPage/components/InfoSection/InfoSectionContainer/components/InfoSectionProperties/index.tsx
+++ src/client/components/LandingPage/components/InfoSection/InfoSectionContainer/components/InfoSectionProperties/index.tsx
@@ -4,15 +4,18 @@ import GridDots from "@/client/components/Svg/GridDots";
 
 const InfoSectionProperties = () => {
 	return (
-		<div className='mt-28 rounded-md'>
+		<div className='lg:mt-28 mt-10 rounded-md 2sm:p-6 lg:p-0'>
 			<div className='flex flex-row items-center justify-between'>
-				<GridDots width='64' height='64' color='#424141' />
+				<div className="2sm:hidden lg:block">
+			     	<GridDots width='64' height='64' color='#424141' />
+				</div>
 				<InfoSectionTitle text={"Move To What Moves You"} />
 			</div>
-			<div className='mt-16'>
+			<p className="lg:hidden text-center mt-12">We connect clients with housing agents that post housing unit for rent, lease or sale on our platform at the best rate. </p>
+			<div className='lg:mt-16'>
 				<Card />
 			</div>
-			<div className='mt-20'>
+			<div className='lg:mt-20 2sm:hidden lg:block'>
 				<InfoSectionTitle text={"Top Reviewed House Units"} />
 			</div>
 		</div>
