2bed2628032bf1c72184dc8f67131debaee2870d
jbrainz <piusunimke@gmail.com>
2023-05-22 16:49:12 +0300

    fix: mobile nav bar[PL-15]

--- src/client/components/LandingPage/components/Header/HeaderContainer/components/HeaderNavbar/MobileNavbar.tsx
+++ src/client/components/LandingPage/components/Header/HeaderContainer/components/HeaderNavbar/MobileNavbar.tsx
@@ -1,30 +1,62 @@
-import { useState } from "react";
 import { HEADER_NAVBAR_DATA } from "../../../../../constants/data";
-import { ControlsClose  } from '@heathmont/moon-icons-tw';
+import { ControlsClose, GenericBurgerRegular } from "@heathmont/moon-icons-tw";
 import NextLink from "@/client/components/NextLink";
+import useMobileHeader from "./useMobileHeader";
+import { Drawer } from "@heathmont/moon-core-tw";
 
+export const MobileNavbar = () => {
+	const { handleToggle, toggle, setToggle } = useMobileHeader();
+	if (!toggle) {
+		return (
+			<div className='flex flex-row items-center justify-between text-white'>
+				<p className='text-center text-xl font-bold 2sm:text-picollo-50 lg:text-white'>
+					LOGO.
+				</p>
+				<GenericBurgerRegular
+					width={40}
+					height={40}
+					color='white'
+					className='lg:hidden'
+					onClick={handleToggle}
+				/>
+			</div>
+		);
+	}
+	return (
+		<div className='h-screen w-full'>
+			<div className='flex flex-col items-end justify-start'>
+				<Hamburguer
+					props={{
+						width: 40,
+						height: 40,
+						color: "white",
+						className: "lg:hidden",
+						onClick: handleToggle,
+					}}
+					toggle={toggle}
+				/>
+			</div>
+			<Drawer
+				open={toggle}
+				setOpen={setToggle}
+				className='flex w-full flex-col items-center justify-start  text-white '
+			>
+				<Drawer.Panel className='mt-20 items-center justify-start space-y-5  p-5'>
+					<NextLink items={HEADER_NAVBAR_DATA} column={true} href='#' />
+				</Drawer.Panel>
+			</Drawer>
+		</div>
+	);
+};
 
 type Props = {
-    onClick: () => void;
-}
+	toggle: boolean;
+	props: React.SVGProps<SVGSVGElement>;
+};
 
-export const MobileNavbar = ({ onClick }: Props) => {
-    
-    return (
-        <div className='h-screen w-full'>
-            <div className='flex flex-col items-end justify-start'>
-                <ControlsClose 
-                    width={40}
-                    height={40}
-                    color='white'
-                    className='lg:hidden'
-                    onClick={onClick}
-                />							
-            </div>	
-            <div className='flex flex-col items-start justify-start space-y-5 w-full text-white p-5 '>
-                <NextLink items={HEADER_NAVBAR_DATA} column={true} href='#' />
-            </div>
-        </div>	
-    )
-}
-
+const Hamburguer = (props: Props) => {
+	if (props.toggle) {
+		return <ControlsClose {...props.props} />;
+	}
+	return <GenericBurgerRegular {...props.props} />;
+};
--- src/client/components/LandingPage/components/Header/HeaderContainer/components/HeaderNavbar/index.tsx
+++ src/client/components/LandingPage/components/Header/HeaderContainer/components/HeaderNavbar/index.tsx
@@ -1,40 +1,28 @@
-import { useState } from 'react';
-import { GenericBurgerRegular, ControlsClose  } from '@heathmont/moon-icons-tw';
 import NextLink from "@/client/components/NextLink";
 import { Button } from "@heathmont/moon-core-tw";
 import { HEADER_NAVBAR_DATA } from "../../../../../constants/data";
-import { MobileNavbar } from './MobileNavbar';
-
+import { MobileNavbar } from "./MobileNavbar";
+import useDeviceType from "@/client/shared/hooks/useDeviceType";
 
 const HeaderNavbar = () => {
-	const [click, setClick] = useState(false);
-	const handleMenuClick = () => {setClick(!click);}
+	const { isMobile } = useDeviceType();
+
+	if (isMobile) {
+		return <MobileNavbar />;
+	}
 
 	return (
-		<>
-			{
-				click 
-					?
-					    <MobileNavbar onClick={handleMenuClick} />
-					:
-						<div className='flex flex-row items-center justify-between text-white'>
-							<p className='text-center text-xl 2sm:text-picollo-50 lg:text-white font-bold'>LOGO.</p>
-							<div className='flex w-[480px] flex-row  items-center 2sm:hidden lg:block'>
-								<NextLink items={HEADER_NAVBAR_DATA} column={false} href='#' />
-							</div>
-							<Button className='hidden lg:block h-[50px] w-[148px]  rounded-xl bg-picollo-50 px-6 py-4'>
-								<p className='text-md font-bold'>Sign Up</p>
-							</Button>
-							<GenericBurgerRegular
-								width={40}
-								height={40}
-								color='white'
-								className='lg:hidden'
-								onClick={handleMenuClick}
-							/>
-						</div>
-			}
-		</>
+		<div className='flex flex-row items-center justify-between text-white'>
+			<p className='text-center text-xl font-bold 2sm:text-picollo-50 lg:text-white'>
+				LOGO.
+			</p>
+			<div className='flex w-[480px] flex-row  items-center 2sm:hidden lg:block'>
+				<NextLink items={HEADER_NAVBAR_DATA} column={false} href='#' />
+			</div>
+			<Button className='hidden h-[50px] w-[148px] rounded-xl  bg-picollo-50 px-6 py-4 lg:block'>
+				<p className='text-md font-bold'>Sign Up</p>
+			</Button>
+		</div>
 	);
 };
 
