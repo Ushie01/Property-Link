e20b05fd71eb4b9445c7756aa1abd829f6a6c37c
Ushie01 <abugodwinaj@gmail.com>
2023-05-22 21:22:05 +0100

    fix: WayToRent section[PL-15]

--- src/client/components/LandingPage/components/WayToRent/index.tsx
+++ src/client/components/LandingPage/components/WayToRent/index.tsx
@@ -16,19 +16,19 @@ const WayToRent = () => {
                 />
             </div>
             <div className="flex flex-row items-center justify-between mt-2">
-                <div className="w-3/6 text-2xl font-thin">
-                    <p>
+                <div className="w-1/2 text-2xl font-thin space-y-52">
+                    <p className="raleway-normal tracking-widest">
                         Unlock endless possibilities and unleash your full potential with a career as a real estate agent with us today, gain access to a wide variety of clients as well as resources to position you for success whether your looking to rent, lease or sell. With flexibility to set your own schedule, the potential for high earnings and the satisfaction of helping people own their homes.
                     </p>
-                    <Button className='h-12 w-64 mt-64 rounded-xl bg-black text-white font-bold'>Sign up as an House Agent</Button>
+                    <Button className='h-12 w-64 rounded-xl bg-black text-white font-bold'>Sign up as an House Agent</Button>
                 </div>
-                <div className="flex flex-col items-end justify-end space-y-5 w-2/5">
+                <div className="flex flex-col items-end justify-end space-y-5 w-1/2">
                     <div className="flex flex-row items-center justify-end w-full space-x-12">
-                        <Image src={Ellipse54} alt="Ellipse54" height={150} width={150} className="mt-32" />
-                        <Image src={Ellipse55} alt="Ellipse55" height={250} width={250} />
+                        <Image src={Ellipse54} alt="Ellipse54" height={200} width={200} className="mt-44" />
+                        <Image src={Ellipse55} alt="Ellipse55" height={300} width={300} />
                     </div>
                     <div>
-                        <Image src={Ellipse56} alt="Ellipse56" height={350} width={350} />
+                        <Image src={Ellipse56} alt="Ellipse56" height={400} width={400} className=""/>
                     </div>
                 </div>
             </div>
