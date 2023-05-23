44a03c1cfd7582eccdd140818bceee32ef3addb2
Ushie01 <abugodwinaj@gmail.com>
2023-05-23 19:11:43 +0100

    fix: updating StarRating component

--- src/client/components/LandingPage/components/ServiceApartment/ServiceApartmentContainer/components/Card.tsx
+++ src/client/components/LandingPage/components/ServiceApartment/ServiceApartmentContainer/components/Card.tsx
@@ -52,7 +52,7 @@ const Card = ({image, type, location, amount, bedroom, bathroom, swimpool}: Card
                 </div>
 
                 <div className='flex flex-row items-center justify-between w-full space-x-4 2sm:mt-1'>   
-                    <Star size="14" starEmpty={1} starFill={5} />
+                    <Star size="14" starFill={3} />
                     <div className='flex flex-row items-center justify-between 2sm:-ml-10 space-x-1 p-2'>
                         <p className='text-picollo-50 lg:text-xl 2sm:text-xl font-bold'>{amount}</p>
                         <p className='text-gray-500'> / Year</p>
--- src/client/components/LandingPage/components/ServiceApartment/ServiceApartmentContainer/components/StarRating.tsx
+++ src/client/components/LandingPage/components/ServiceApartment/ServiceApartmentContainer/components/StarRating.tsx
@@ -4,19 +4,23 @@ import StarEmpty from '@/client/components/Svg/StarEmpty';
 type StarProps = {
   size: string;
   starFill: number;
-  starEmpty: number;
 };
 
-const Star = ({ size, starFill, starEmpty }: StarProps) => (
-  <div className="flex flex-row space-x-1">
-    {[...Array(starFill)].map((_, index) => (
-      <StarFill key={index} height={size} width={size} color="#FFB319" />
-    ))}
-    {[...Array(starEmpty)].map((_, index) => (
-      <StarEmpty key={index} height={size} width={size} color="#FFB319" />
-    ))}
-  </div>
-);
+const Star = ({ size, starFill }: StarProps) => {
+  // Calculate the number of empty stars
+  const starEmpty = 5 - starFill; 
+
+  return (
+    <div className="flex flex-row space-x-1">
+      {[...Array(starFill)].map((_, index) => (
+        <StarFill key={index} height={size} width={size} color="#FFB319" />
+      ))}
+      {[...Array(starEmpty)].map((_, index) => (
+        <StarEmpty key={index} height={size} width={size} color="#FFB319" />
+      ))}
+    </div>
+  );
+};
 
 export default Star;
 
